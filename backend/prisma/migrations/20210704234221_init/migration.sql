4523084ea2e2e7ac1e13ead88c5b4b25753417d6
pluswing <toshiyuki@pluswing.co.jp>
2021-07-06 07:48:14 +0900

    #37 mysql, prisma導入

---
+++ backend/prisma/migrations/20210704234221_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `loginId` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
