1a468eae9118f0a1b853351e5c701c1aa681f992
pluswing <toshiyuki@pluswing.co.jp>
2021-07-09 15:11:51 +0900

    #41 db.ts書き換え完了

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,58 +2,16 @@ import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 
-import { PrismaClient, User } from '@prisma/client';
+import {
+  AccessToken,
+  Account,
+  OrderItem,
+  OrderParent,
+  PrismaClient,
+  Product,
+  User,
+} from '@prisma/client';
 const prisma = new PrismaClient();
-/*
-export interface User {
-  id: number;
-  loginId: string;
-  password: string;
-}
-*/
-interface AccessToken {
-  userId: number;
-  accessToken: string;
-}
-
-export interface Account {
-  userId: number;
-  stripeAccountId: string;
-  draft: boolean;
-}
-
-export interface Product {
-  id: number;
-  userId: number;
-  name: string;
-  amount: number;
-  url: string;
-}
-
-export interface OrderParent {
-  id: number;
-  transferGroupId: string; // unique
-  amount: number;
-  status: 'order' | 'paid' | 'refund';
-  createdAt: Date;
-  paidAt: Date | null;
-  chargeId: string | null;
-}
-
-export interface OrderItem {
-  id: number;
-  parentId: number;
-  productId: number;
-  // userId: number;
-  transfer: number;
-  fee: number; // transfer + fee = product.amount
-  transferId: string | null;
-}
-
-export interface Order {
-  parent: OrderParent;
-  items: OrderItem[];
-}
 
 // util
 const md5str = (str: string): string => {
@@ -179,16 +137,13 @@ export const removeDraft = async (account: Account): Promise<Account> => {
 };
 
 // Product
-let products: Product[] = [];
 // 商品登録
-export const registerProduct = (
+export const registerProduct = async (
   user: User,
   name: string,
   amount: number,
   url: string
-): Product => {
-  // MEMO validation
-
+): Promise<Product> => {
   if (!name) {
     throw new Error('required name');
   }
@@ -202,32 +157,44 @@ export const registerProduct = (
     throw new Error('required url');
   }
 
-  const p: Product = {
-    id: products.length + 1,
-    userId: user.id,
-    name,
-    amount,
-    url,
-  };
-  products.push(p);
+  const p = await prisma.product.create({
+    data: {
+      userId: user.id,
+      name,
+      amount,
+      url,
+    },
+  });
   return p;
 };
 
 // 商品一覧（ユーザ毎）
-export const listProductByUser = (user: User): Product[] => {
-  return products.filter((p) => p.userId == user.id);
+export const listProductByUser = async (user: User): Promise<Product[]> => {
+  return await prisma.product.findMany({
+    where: {
+      user,
+    },
+  });
 };
 
 // 商品検索（全部)
-export const listProducts = (query: string): Product[] => {
+export const listProducts = async (query: string): Promise<Product[]> => {
   if (!query) {
-    return products;
+    return await prisma.product.findMany();
   }
-  return products.filter((p) => p.name.indexOf(query) != -1);
+  return await prisma.product.findMany({
+    where: {
+      name: {
+        contains: query,
+      },
+    },
+  });
 };
 
-export const findProduct = (id: number): Product => {
-  const p = products.find((p) => p.id == id);
+export const findProduct = async (id: number): Promise<Product> => {
+  const p = await prisma.product.findFirst({
+    where: { id },
+  });
   if (!p) {
     throw new Error('product not found');
   }
@@ -235,121 +202,118 @@ export const findProduct = (id: number): Product => {
 };
 
 // Order
-let orderParents: OrderParent[] = [];
-export const createOrder = (
+export const createOrder = async (
   amount: number,
   transferGroupId: string
-): OrderParent => {
-  const o: OrderParent = {
-    id: orderParents.length + 1,
-    amount,
-    transferGroupId,
-    // userId: number;
-    createdAt: new Date(),
-    status: 'order',
-    paidAt: null,
-    chargeId: null,
-  };
-  orderParents.push(o);
+): Promise<OrderParent> => {
+  const o = await prisma.orderParent.create({
+    data: {
+      amount,
+      transferGroupId,
+      // userId: number;
+      createdAt: new Date(),
+      status: 'ORDER',
+      paidAt: null,
+      chargeId: null,
+    },
+  });
   return o;
 };
 
-export const refundOrder = (order: OrderParent): void => {
-  order.status = 'refund';
+export const refundOrder = async (order: OrderParent): Promise<void> => {
+  await prisma.orderParent.update({
+    where: { id: order.id },
+    data: {
+      status: 'REFUND',
+    },
+  });
 };
 
-export const paidOrder = (transferGroupId: string, chargeId: string): void => {
-  const o = orderParents.find((o) => o.transferGroupId == transferGroupId);
+export const paidOrder = async (
+  transferGroupId: string,
+  chargeId: string
+): Promise<void> => {
+  const o = await prisma.orderParent.update({
+    where: {
+      transferGroupId,
+    },
+    data: {
+      status: 'PAID',
+      paidAt: new Date(),
+      chargeId,
+    },
+  });
+  // TODO 要チェック
   if (!o) {
     throw new Error('order not found.');
   }
-  o.status = 'paid';
-  o.paidAt = new Date();
-  o.chargeId = chargeId;
 };
 
-export const findOrderByTransferGroup = (transferGroupId: string): Order => {
-  const parent = orderParents.find((o) => o.transferGroupId == transferGroupId);
+export const findOrderByTransferGroup = async (
+  transferGroupId: string
+): Promise<OrderParent> => {
+  const parent = await prisma.orderParent.findFirst({
+    select: {
+      id: true,
+      transferGroupId: true,
+      amount: true,
+      status: true,
+      createdAt: true,
+      paidAt: true,
+      chargeId: true,
+      orderItems: true,
+    },
+    where: { transferGroupId },
+  });
   if (!parent) {
     throw new Error('order not found.');
   }
-  return findOrder(parent.id);
+  return parent;
 };
 
-export const findOrder = (id: number): Order => {
-  const parent = orderParents.find((o) => o.id == id);
-  if (!parent) {
-    throw new Error('order not found.');
-  }
-  const items = orderItems.filter((i) => i.parentId == parent.id);
-
-  return {
-    parent,
-    items,
-  } as Order;
+export const listOrderParent = async (): Promise<OrderParent[]> => {
+  return await prisma.orderParent.findMany({
+    where: { status: 'PAID' },
+  });
 };
 
-export const listOrderParent = (): OrderParent[] => {
-  return orderParents.filter((o) => o.status == 'paid');
-};
-
-let orderItems: OrderItem[] = [];
-export const addOrderItem = (
+export const addOrderItem = async (
   parent: OrderParent,
   product: Product,
   transferRatio: number = 0.9
-) => {
+): Promise<OrderItem> => {
   const transfer = Math.ceil(product.amount * transferRatio);
   const fee = product.amount - transfer;
 
-  const item: OrderItem = {
-    id: orderItems.length + 1,
-    parentId: parent.id,
-    productId: product.id,
-    transfer,
-    fee,
-    transferId: null,
-  };
-  orderItems.push(item);
+  const item = await prisma.orderItem.create({
+    data: {
+      parentId: parent.id,
+      productId: product.id,
+      transfer,
+      fee,
+      transferId: null,
+    },
+  });
   return item;
 };
 
-export const saveTransfer = (item: OrderItem, transferId: string): void => {
-  item.transferId = transferId;
+export const saveTransfer = async (
+  item: OrderItem,
+  transferId: string
+): Promise<void> => {
+  await prisma.orderItem.update({
+    where: { id: item.id },
+    data: { transferId },
+  });
 };
 
-export const listOrder = (user: User): OrderItem[] => {
-  const productIds = listProductByUser(user).map((p) => p.id);
-  const items = orderItems.filter((o) => productIds.includes(o.productId));
+export const listOrder = async (user: User): Promise<OrderItem[]> => {
+  const items = await prisma.orderItem.findMany({
+    where: {
+      product: {
+        user,
+      },
+    },
+  });
   return items;
-};
-
-// general
-export const saveData = () => {
-  fs.writeFileSync(
-    'data.json',
-    JSON.stringify({
-      users,
-      accessTokens,
-      accounts,
-      products,
-      orderParents,
-      orderItems,
-    })
-  );
-  console.log('*** DONE SAVE ***');
-};
-
-export const loadData = () => {
-  if (!fs.existsSync('data.json')) {
-    return;
-  }
-  const data = JSON.parse(fs.readFileSync('data.json').toString());
-  users = data.users || [];
-  accessTokens = data.accessTokens || [];
-  accounts = data.accounts || [];
-  products = data.products || [];
-  orderParents = data.orderParents || [];
-  orderItems = data.orderItems || [];
-  console.log('*** DONE LOAD ***');
 };
